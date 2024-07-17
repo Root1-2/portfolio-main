0038156015ac70344e6365adf5bed424eafae203
John Doe <abdullahalmasrur8@gmail.com>
2024-07-18 02:01:24 +0600

    another forgotten commit

--- src/list/skills.js
+++ src/list/skills.js
@@ -2,57 +2,46 @@ const skills = [
   {
     name: "React",
     imgPath: "/skills/react.png",
-    desc: "",
   },
   {
     name: "JavaScript",
     imgPath: "/skills/js.png",
-    desc: "",
   },
   {
     name: "PHP",
     imgPath: "/skills/PHP.svg",
-    desc: "",
   },
   {
     name: "Flutter",
     imgPath: "/skills/flutter.svg",
-    desc: "",
   },
   {
     name: "Dart",
     imgPath: "/skills/dart.svg",
-    desc: "",
   },
   {
     name: "Database",
     imgPath: "/skills/db.jpg",
-    desc: "",
   },
   {
     name: "Electron",
     imgPath: "/skills/electron.jpg",
-    desc: "",
   },
   {
     name: "HTML",
     imgPath: "/skills/html.svg",
-    desc: "",
   },
   {
     name: "CSS",
     imgPath: "/skills/css.svg",
-    desc: "",
   },
   {
     name: "Python",
     imgPath: "/skills/python.svg",
-    desc: "",
   },
   {
     name: "C++",
     imgPath: "/skills/c++.svg",
-    desc: "",
   },
 ];
 
