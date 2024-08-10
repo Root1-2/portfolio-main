04bb930027c637e2b981a99b2363d9aebb03ba1e
John Doe <abdullahalmasrur8@gmail.com>
2024-08-11 01:48:20 +0600

    routing setup, folder structure, tailwind css init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/landing";
+import Project from "./pages/Project";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  const x = 5;
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Landing />} />
+          <Route path="homepage" element={<Landing />} />
+          <Route path="project/:id" element={<Project />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
