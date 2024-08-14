f5b0bbdf331e4cca199fcd3b35cd826e1e84159e
John Doe <abdullahalmasrur8@gmail.com>
2024-08-15 01:12:42 +0600

    #

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,9 +9,9 @@ function Projects() {
             Projects
           </p>
           <div className="mt-14 flex h-[70vh] flex-wrap gap-20 overflow-y-auto">
-            {projects.map((project, index) => (
+            {projects.map((project) => (
               <div
-                key={index}
+                key={project.id}
                 className="group relative h-44 w-80 scale-90 transform rounded-lg bg-gradient-to-r from-purple-500 to-indigo-600 transition duration-300 ease-in-out hover:scale-100 hover:bg-gradient-to-r hover:from-pink-500 hover:to-purple-700"
               >
                 <img
@@ -19,9 +19,10 @@ function Projects() {
                   className="absolute left-0 top-0 h-full w-full rounded-lg object-cover transition-opacity duration-300 ease-in-out group-hover:opacity-50"
                   alt={project.name}
                 />
-                <p className="absolute inset-0 flex items-center justify-center text-2xl font-semibold text-slate-50 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
-                  {project.name}
-                </p>
+                <div className="absolute inset-0 flex flex-col items-center justify-center text-slate-50 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+                  <p className="text-2xl font-semibold">{project.name}</p>
+                  <p className="text-base">{project.tools}</p>
+                </div>
               </div>
             ))}
           </div>
