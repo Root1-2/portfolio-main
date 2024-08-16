ee68f8a269e892d5a063b1840fd4428001c8b235
John Doe <abdullahalmasrur8@gmail.com>
2024-08-16 18:34:50 +0600

    #

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import projects from "../list/projects";
 
 function Projects() {
@@ -10,20 +11,24 @@ function Projects() {
           </p>
           <div className="mt-14 flex h-[70vh] flex-wrap gap-20 overflow-y-auto">
             {projects.map((project) => (
-              <div
-                key={project.id}
-                className="group relative h-44 w-80 scale-90 transform rounded-lg bg-gradient-to-r from-purple-500 to-indigo-600 transition duration-300 ease-in-out hover:scale-100 hover:bg-gradient-to-r hover:from-pink-500 hover:to-purple-700"
-              >
-                <img
-                  src={project.thumbnail}
-                  className="absolute left-0 top-0 h-full w-full rounded-lg object-cover transition-opacity duration-300 ease-in-out group-hover:opacity-50"
-                  alt={project.name}
-                />
-                <div className="absolute inset-0 flex flex-col items-center justify-center text-slate-50 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
-                  <p className="text-2xl font-semibold">{project.name}</p>
-                  <p className="mt-2 text-center text-base">{project.tools}</p>
+              <NavLink to={`/project/${project.id}`}>
+                <div
+                  key={project.id}
+                  className="group relative h-44 w-80 scale-90 transform cursor-pointer rounded-lg bg-gradient-to-r from-purple-500 to-indigo-600 transition duration-300 ease-in-out hover:scale-100 hover:bg-gradient-to-r hover:from-pink-500 hover:to-purple-700"
+                >
+                  <img
+                    src={project.thumbnail}
+                    className="absolute left-0 top-0 h-full w-full rounded-lg object-cover transition-opacity duration-300 ease-in-out group-hover:opacity-50"
+                    alt={project.name}
+                  />
+                  <div className="absolute inset-0 flex flex-col items-center justify-center text-slate-50 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+                    <p className="text-2xl font-semibold">{project.name}</p>
+                    <p className="mt-2 text-center text-base">
+                      {project.tools}
+                    </p>
+                  </div>
                 </div>
-              </div>
+              </NavLink>
             ))}
           </div>
         </div>
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,8 +1,45 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
+import bg from "/banner-bg.png";
+import Header from "../components/Header";
 
 function Project() {
-  let { id } = useParams();
-  return <div>Project with ID</div>;
+  // let { id } = useParams();
+  return (
+    <div className="relative">
+      <div
+        className="absolute inset-0 h-screen bg-cover bg-center"
+        style={{ backgroundImage: `url(${bg})` }}
+      >
+        <Header />
+        <div className="pt-24 text-white">
+          <div className="mx-52">
+            <div className="mb-5 flex justify-between">
+              <div className="flex">
+                <div className="h-44 w-80 bg-red-400"></div>
+                <div className="ms-10">
+                  <p className="mb-2 text-3xl font-semibold text-slate-200">
+                    Project Name
+                  </p>
+                  <p className="mb-2 text-2xl text-slate-100">Project Tools</p>
+                  <p>Project Links</p>
+                </div>
+              </div>
+              <div>
+                <p>Project Created</p>
+                <p>Project Type</p>
+              </div>
+            </div>
+
+            <div className="border-b border-gray-600"></div>
+
+            <div className="mt-5">
+              <p className="text-4xl font-semibold">Description</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Project;
