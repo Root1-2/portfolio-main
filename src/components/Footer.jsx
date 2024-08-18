9278fdf442b98a9465b9e687e7f8b6e750024b8b
John Doe <abdullahalmasrur8@gmail.com>
2024-08-18 23:26:28 +0600

    #

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,8 @@ function Footer() {
           <a
             href="#landing"
             class="mb-4 flex items-center space-x-3 sm:mb-0 rtl:space-x-reverse"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <img src={icon} class="h-8 pt-2" alt="Logo" />
             <span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
@@ -18,7 +20,12 @@ function Footer() {
           </a>
           <ul class="mb-6 flex flex-wrap items-center gap-5 text-sm font-medium text-gray-400 sm:mb-0">
             <li>
-              <a href="https://github.com/Root1-2" class="me-4 md:me-6">
+              <a
+                href="https://github.com/Root1-2"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="me-4 md:me-6"
+              >
                 <FaGithub title="GitHub Profile" size={30} />
               </a>
             </li>
@@ -26,25 +33,27 @@ function Footer() {
               <a
                 href="https://www.linkedin.com/in/abdullah-al-masrur-839000222/"
                 class="me-4 hover:underline md:me-6"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 <FaLinkedinIn title="LinkedIn Profile" size={30} />
               </a>
             </li>
             <li>
-              <a href="/CV.pdf" class="me-4 hover:underline md:me-6">
+              <a
+                href="/CV.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="me-4 hover:underline md:me-6"
+              >
                 <IoDocumentsOutline title="CV" size={30} />
-              </a>
-            </li>
-            <li>
-              <a href="#" class="me-4 hover:underline md:me-6">
-                <FaGithub size={30} />
               </a>
             </li>
           </ul>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8 dark:border-gray-700" />
         <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
+          © 2024{" "}
           <a href="https://flowbite.com/" class="hover:underline">
             Portfolio
           </a>
