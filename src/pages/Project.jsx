3bf284655620ef91745bcff6d71fc134521a1d47
John Doe <abdullahalmasrur8@gmail.com>
2024-08-19 14:34:15 +0600

    #

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -62,7 +62,7 @@ function Project() {
                   </div>
                 </div>
               </div>
-              <div className="absolute bottom-60 right-5 mt-5 justify-end sm:static sm:mt-0 sm:block">
+              <div className="absolute bottom-44 right-5 mt-5 justify-end sm:static sm:mt-0 sm:block">
                 <p className="rounded-full bg-cyan-950 px-3 py-2 text-base font-semibold text-slate-100 sm:px-5 sm:py-3">
                   {project.type}
                 </p>
