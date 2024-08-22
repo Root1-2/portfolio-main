8be51568aa5482b93d11a467507d438e90fa5287
John Doe <abdullahalmasrur8@gmail.com>
2024-08-22 14:20:43 +0600

    #

--- src/components/LandingBody.jsx
+++ src/components/LandingBody.jsx
@@ -6,7 +6,7 @@ function LandingBody() {
   return (
     <section id="landing">
       <div className="flex h-screen flex-wrap items-center justify-between gap-3 p-5 text-white sm:gap-0 sm:p-20">
-        <div className="animate__animated animate__backInLeft w-2/3 sm:w-1/2">
+        <div className="animate__animated animate__backInLeft animate__fast w-2/3 sm:w-1/2">
           <p className="mb-3 sm:mb-10">
             <span className="mb-2 border border-gray-100 bg-custom-gradient px-1 py-3 font-mono text-sm sm:px-3 sm:py-5 sm:text-4xl">
               Welcome To My Portfolio
@@ -18,12 +18,12 @@ function LandingBody() {
           <p className="mb-5 text-xl font-thin sm:text-6xl">Web Developer</p>
           <p className="mb-5 text-justify text-sm font-thin text-slate-300 sm:text-xl">
             Highly motivated individual seeking for a challenging position in
-            the computer science field. My strong command in Python, C, C++, PHP
-            and with other languages has been proven by successfully designing,
-            developing and implementing various state of the art projects. Eager
-            to apply these gained knowledge and experience towards critical
-            problem solving in an innovative way for the success of the
-            organization.
+            the computer science field. My strong command in React, HTML, CSS,
+            PHP and with other languages has been proven by successfully
+            designing, developing and implementing various state of the art
+            projects. Eager to apply these gained knowledge and experience
+            towards critical problem solving in an innovative way for the
+            success of the organization.
           </p>
           <button className="rounded-lg border p-2 transition hover:bg-slate-800 sm:p-5">
             <span className="flex gap-3 text-xl">
@@ -31,7 +31,7 @@ function LandingBody() {
             </span>
           </button>
         </div>
-        <div className="animate__animated animate__backInRight w-1/4 text-center">
+        <div className="animate__animated animate__backInRight animate__fast w-1/4 text-center">
           <img src={banner} className="animate-updown" alt="bannerLogo" />
         </div>
       </div>
