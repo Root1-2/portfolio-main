18b449b6b2b7a6105faf292d6184499bd07b70b9
John Doe <abdullahalmasrur8@gmail.com>
2024-09-25 23:27:28 +0600

    skills

--- src/list/skills.js
+++ src/list/skills.js
@@ -1,23 +1,58 @@
 const skills = [
   {
     name: "React",
-    imgPath: "/react.svg",
+    imgPath: "/react.png",
+    desc: "",
   },
   {
     name: "JavaScript",
-    imgPath: "/js.svg",
+    imgPath: "/js.png",
+    desc: "",
   },
   {
     name: "PHP",
     imgPath: "/PHP.svg",
+    desc: "",
   },
   {
     name: "Flutter",
     imgPath: "/flutter.svg",
+    desc: "",
   },
   {
     name: "Dart",
     imgPath: "/dart.svg",
+    desc: "",
+  },
+  {
+    name: "Electron",
+    imgPath: "/electron.jpg",
+    desc: "",
+  },
+  {
+    name: "HTML",
+    imgPath: "/html.svg",
+    desc: "",
+  },
+  {
+    name: "CSS",
+    imgPath: "/css.svg",
+    desc: "",
+  },
+  {
+    name: "Python",
+    imgPath: "/python.svg",
+    desc: "",
+  },
+  {
+    name: "Dart",
+    imgPath: "/dart.svg",
+    desc: "",
+  },
+  {
+    name: "Dart",
+    imgPath: "/dart.svg",
+    desc: "",
   },
 ];
 
