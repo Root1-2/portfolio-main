1e07a9a265b7049f12a7b986e3a3abb4911bfe2f
John Doe <abdullahalmasrur8@gmail.com>
2024-09-27 23:11:40 +0600

    #

--- src/list/subskills.js
+++ src/list/subskills.js
@@ -11,8 +11,33 @@ const subskills = [
   },
   {
     for: "React",
+    name: "React Hook Form",
+    imgPath: "/subskills/reacthookform.png",
+  },
+  {
+    for: "React",
+    name: "Styled Components",
+    imgPath: "/subskills/sComp.png",
+  },
+  {
+    for: "React",
     name: "Redux",
     imgPath: "/subskills/redux.svg",
+  },
+  {
+    for: "JavaScript",
+    name: "JQuery",
+    imgPath: "/subskills/jquery.png",
+  },
+  {
+    for: "JavaScript",
+    name: "DOM",
+    imgPath: "/subskills/dom.png",
+  },
+  {
+    for: "JavaScript",
+    name: "AJAX",
+    imgPath: "/subskills/ajax.png",
   },
   {
     for: "CSS",
