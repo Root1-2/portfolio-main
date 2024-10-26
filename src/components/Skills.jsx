a264d655c149d8cbd47f53a3ad7dd127ec9c9fb4
root12 <abdullahalmasrur8@gmail.com>
2024-10-26 20:57:18 +0600

    responsive fixed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,16 +13,16 @@ function Skills() {
   return (
     <section id="skills">
       <div className="h-screen pt-20 text-white">
-        <div className="mx-7 rounded-full bg-neutral-900 p-7 md:mx-20 md:p-10">
-          <p className="mb-10 text-center text-3xl font-semibold text-slate-100 md:text-4xl">
+        <div className="mx-7 rounded-full bg-neutral-900 p-1 md:mx-20 md:p-10">
+          <p className="mb-10 pt-4 text-center text-xl font-semibold text-slate-100 md:text-4xl">
             Skills
           </p>
           <Carousel data={skills} onClick={setSelectedSkill} />
         </div>
 
         {selectedSkill && filteredSubskills.length > 0 && (
-          <div className="animate__animated animate__backInUp m:p-10 mx-7 mt-10 rounded-full bg-neutral-900 p-7 md:mx-20">
-            <p className="mb-10 text-center text-3xl font-semibold text-slate-100 md:text-4xl">
+          <div className="animate__animated animate__backInUp mx-7 mt-7 rounded-full bg-neutral-900 p-1 md:mx-20 md:p-10">
+            <p className="mb-10 pt-4 text-center text-xl font-semibold text-slate-100 md:text-4xl">
               Sub-Skills for {selectedSkill}
             </p>
             <Carousel data={filteredSubskills} />
